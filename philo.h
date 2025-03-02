155c6f0387818b5b80fd54925a29c9ded8a7a45e
Rayane Lamlaika <raynlamlaika.lamlaikarayane@gmail.com>
2025-03-02 17:56:51 +0000

    initialization

---
+++ philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/02 16:44:49 by rlamlaik          #+#    #+#             */
+/*   Updated: 2025/03/02 17:48:54 by rlamlaik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<pthread.h>
+#include <sys/time.h>
+
+#endif
