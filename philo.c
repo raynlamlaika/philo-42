136920393b740c875c326d4a206ee71bf2c97ae3
Rayane Lamlaika <raynlamlaika.lamlaikarayane@gmail.com>
2025-06-11 06:26:32 +0100

    helo

--- philo.c
+++ philo.c
@@ -1,117 +1,237 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2025/03/02 16:45:43 by rlamlaik          #+#    #+#             */
-/*   Updated: 2025/05/21 19:14:41 by rlamlaik         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 02:11:46 by rlamlaik          #+#    #+#             */
+/*   Updated: 2025/06/11 06:18:02 by rlamlaik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "philo.h"
 
-void clear(t_data *philo)
+static int	ft_overflow(int sing)
 {
-	int	i;
-
-	i = 0;
-	if (philo->forks)
-		free(philo->forks);
-	if (philo)
-		free(philo);
-	exit(1);
+	if (sing == 1)
+		return (-1);
+	return (0);
 }
 
-
-int init_forks(t_data *philos)
-{
-	int	i;
-
-	i = 0;
-	philos->forks = malloc(sizeof(pthread_mutex_t) * philos->nbr_philos);
-	if (!philos->forks)
-		return (write(2, "malloc are filed\n", 17), 1);
-	while (philos->nbr_philos > i)
-    {
-		// check fail it should ddestroy
-		// cech that case where the first will be created but sec it will faild
-		pthread_mutex_init(&philos->forks[i],NULL);
-        // printf("Fork %d initialized at address: %p\n", i, (void *)&philos->forks[i]);
-        i++;
-    }
-	return (1);
-}
-
-
-
-int destroy_forks(t_data *philos)
-{
-	int i = 0;
-
-	while (i < philos->nbr_philos)
-    {
-		pthread_mutex_destroy(&philos->forks[i]);
-        // Optional debug print:
-        // printf("Fork %d destroyed at address: %p\n", i, (void *)&philos->forks[i]);
-        i++;
-    }
-	return 1;
-}
-int	init_philos(t_data	*info)
-{
-	int	i;
-
-	i = 0;
-	info->philos = malloc(sizeof(t_philo) * info->nbr_philos);
-	if (!info->philos)
-		return (write(2, "failed in creat of philo [allocation]\n", 38),clear(info),0);
-	while (i < info->nbr_philos)
-	{
-		info->philos[i].id = i + 1;
-		info->philos[i].right_fork = &info->forks[(i + 1) % info->nbr_philos];
-		info->philos[i].left_fork = &info->forks[i];
-		info->philos[i].last_eat = get_time(); // time to start
-		info->philos[i].max_eat = info->philo_must_eat;
-        info->philos[i].data = info;
-		
-
-
-        // printf("id: %d  right fork %p left one %p\n",info->philos[i].id ,info->philos[i].right_fork , info->philos[i].left_fork);
-		// info->philos[i].times_ate = 0;
-		i++;
-	}
-	int y= 0;
-
-	while (y < info->nbr_philos)
-	{
-		if (pthread_create(&info->philos[y].thread, NULL, philo_routine, &info->philos[y]) != 0)
-				return (write(2, "Thread creation failed\n", 23), clear(info), 0);
-		y++;
-	}
-	return (1);
-}
-
-int	main(int ac, char **av)
+int	ft_atoi(const char *str)
 {
 	int		i;
-	t_data	*philos;
+	int		sing;
+	long	rs;
 
-	philos = malloc(sizeof(*philos));
-	philos->philo_must_eat = -1 ; // default init
-	parssing(ac, av, philos);
-	printf("\nparse step: number of philos :%d --> time to die :%d --> time to eat :%d --> time to sleep :%d {philos must eat : %d}\n\n\n"
-		, philos->nbr_philos, philos->time_to_die, philos->time_to_eat ,philos->time_to_sleep, philos->philo_must_eat);
-	init_forks(philos);
-	init_philos(philos);
-	//die(philos);
-    for (i = 0; i < philos->nbr_philos; i++)
-        pthread_join(philos->philos[i].thread, NULL);
-    
-    destroy_forks(philos);
-	// print_philos(philos);
-	free(philos->forks);
-	free(philos);
+	i = 0;
+	rs = 0;
+	sing = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sing *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (rs > (9223372036854775807 - str[i] - 48) / 10)
+			return (ft_overflow(sing));
+		rs = rs * 10 + str[i] - '0';
+		i++;
+	}
+	return ((int )rs * sing);
+}
+
+int ft_isdigit(int n)
+{
+	while(n >= '0' && n <= '9')
+		return 1;
+	return 0;
+}
+
+void add_to(t_data *data, int ac,char** av)
+{
+	if (ac == 1)
+		data->number_of_philos = ft_atoi(av[ac]);
+	else if (ac == 2)
+		data->time_to_die = ft_atoi(av[ac]);
+	else if (ac == 3)
+		data->time_to_eat = ft_atoi(av[ac]);
+	else if (ac == 4)
+		data->time_to_sleep = ft_atoi(av[ac]);
+	else if (av[ac])
+		data->number_of_times_each_philo_must_eat = ft_atoi(av[ac]);
+	else
+		data->number_of_times_each_philo_must_eat = -1;
+}
+
+int parssing(char **av, int ac, t_data*data)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if (ac < 5 || ac >= 7)
+		return (printf("not enoght args\n"),0);
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (ft_isdigit(av[i][j]) == 0 && av[i][j] != '+')
+				return (printf("invalid arg :`%s`: philosopher\n", av[i]), 0);
+			if (av[i][j] == '+')
+			{
+				if (av[i][++j] == '+')
+					return (printf("invalid arg :`%s`: philosopher\n", av[i]), 0);
+			}
+			j++;
+		}
+		add_to(data, i , av);
+		i++;
+	}
+	return (1);
+}
+
+void print_data(t_data *data)
+{
+    printf("Number of philosophers: %zu\n", data->number_of_philos);
+    printf("Time to die: %zu ms\n", data->time_to_die);
+    printf("Time to eat: %zu ms\n", data->time_to_eat);
+    printf("Time to sleep: %zu ms\n", data->time_to_sleep);
+    printf("Number of times each philosopher must eat: %zu\n",
+           data->number_of_times_each_philo_must_eat);
+}
+
+int	init_forks(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->number_of_philos);
+	if (!data->forks)
+		return (write(2, "malloc are filed\n", 17), 1);
+	while (data->number_of_philos > i)
+    {
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+			return (free(data), free(data->forks), exit(1), 0);
+        i++;
+    }
+	return (1);
+}
+
+long get_time()
+{
+    struct timeval time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		return(-1);
+    return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+int stoped(t_data*data)
+{
+	//|| data->philo->eated >= data->number_of_philos
+	if (data->dead)
+		return (0);
+	return (1);
+}
+
+void	takefork(t_philo *philo)
+{
+	pthread_mutex_lock(philo->left_fork);
+	printf("time : `%ld` id: %zu has taken a fork\n", (get_time() - philo->data->start), philo->id);
+	pthread_mutex_lock(philo->right_fork);
+	printf("time : `%ld` id: %zu has taken a fork\n", (get_time() - philo->data->start), philo->id);
+}
+
+void eating(t_philo* philo)
+{
+	printf("time : `%ld` id: %zu is eating\n", (get_time() - philo->data->start) ,philo->id);
+	philo->data->start = get_time() - philo->data->start;
+}
+	
+void sleeping(t_philo* philo)
+{
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+	usleep(philo->data->time_to_sleep);
+}
+
+
+void die(t_philo* philo)
+{
+	if (philo->data->time_to_die > philo->data->time_to_sleep)
+		philo->data->dead = 1;
+		
+}
+
+
+void *philo_routine(void *philo_c)
+{
+	t_philo* philo = (t_philo *) philo_c;
+	while (stoped(philo->data))
+	{
+		takefork(philo);
+		eating(philo);
+		sleeping(philo);
+		die(philo);
+	}
+	return NULL;
+}
+
+int init_philo(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->philo = malloc (sizeof(t_philo));
+	if (!data->philo)
+		return (free(data->philo), free(data), exit(1), 0);
+	while (i < data->number_of_philos)
+	{
+		data->philo->left_fork = &data->forks[i];
+		data->philo->right_fork = &data->forks[(i + 1) % data->number_of_philos];
+		data->philo->id = i;
+		i++;
+	}
+	data->start = get_time();
+	if (data->start == -1)
+		return (free(data->philo), free(data), exit(1), 0);
+	i = 0;
+	while (i < data->number_of_philos)
+	{
+		// data->philo[i];
+		if (pthread_create(&data->philo[i].philo, NULL, philo_routine, &data->philo[i]) != 0)
+				return (write(2, "Thread creation failed\n", 23), 0);
+		i++;
+	}
+	return (1);
+}
+
+void stating(t_data *data)
+{
+	init_forks(data);
+	data->dead = 0;
+	init_philo(data);
+		
+}
+
+int main(int ac, char**av)
+{
+	t_data	*data;
+
+	data = malloc (sizeof(t_data));
+	if (parssing(av, ac, data) == 0)
+		return (free(data), 0);
+	print_data(data);
+	stating(data);
+
+
 	return (0);
+	
 }
--- philo.h
+++ philo.h
@@ -5,10 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/02 16:44:49 by rlamlaik          #+#    #+#             */
-/*   Updated: 2025/05/18 06:02:11 by rlamlaik         ###   ########.fr       */
+/*   Created: 2025/06/11 02:12:31 by rlamlaik          #+#    #+#             */
+/*   Updated: 2025/06/11 06:08:07 by rlamlaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef PHILO_H
 # define PHILO_H
@@ -21,54 +22,32 @@
 #include <sys/time.h>
 #include <limits.h>
 
-
 typedef struct s_data t_data;
+typedef struct s_philo t_philo;
 
 typedef struct s_philo
 {
-	int				id;
-	int				meals_eaten;
-	int				max_eat;
-	pthread_t		thread;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
-	unsigned long	last_eat;
-	t_data			*data;
-}					t_philo;
+	pthread_t			philo;
+	size_t				id;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
+	unsigned int		last_time_eat;
+	unsigned int		eated;
+	t_data				*data;
+}		t_philo;
 
 typedef struct s_data
 {
-	int				nbr_philos;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				philo_must_eat;
-	int         	simulation_stopped;
-	pthread_mutex_t print_mutex;
-	int				someone_died;
-	unsigned long	start_time;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	dead;
-	pthread_mutex_t	write_mutex;
-
-	t_philo			*philos;
-}					t_data;
-
-
-int	check_args(int ac, t_data *philos);
-int	parssing(int ac, char **av, t_data *philos);
-void *philo_routine(void *arg);
-
-long get_time();
-/*utils from libft*/
-int	ft_atoi(const char *str, t_data *philos);
-char	*ft_itoa(int n);
-int	ft_strncmp(const char *str1, const char *str2, size_t num);
-int	ft_isdigit(int c);
-size_t	ft_strlen(const char *str);
-
-/*helpersss*/
-void	print_philos(t_data	*info);
+	t_philo				*philo;
+	pthread_mutex_t		*forks;
+	size_t				number_of_philos;
+	size_t				time_to_die;
+	size_t				time_to_eat;
+	size_t				time_to_sleep;
+	size_t				number_of_times_each_philo_must_eat;
+	int					dead;
+	unsigned int		start;
+}		t_data;
 
 
 #endif
